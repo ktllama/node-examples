a2391860ddd3689fe1663bf58e6403f6171d07cd
Katie <costantinkp@gmail.com>
2022-02-21 18:13:02 -0500

    asynch error handle callback

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const rect = require('./rectangle');
 //now this file knows to look in the same folder as itself for a js file named rectangle
+//now rect is expecting a function not an object
 
 function solveRect(length, width) {
     console.log(`Solving for rectangle with dimensions: ${length}, ${width}`);
 
-    if (length <= 0 || width <= 0){
-        console.log(`Rectangle dimensions must be greated than zero. Received: ${length}, ${width}`);
-    } else {
-        console.log(`Area of rectangle: ${rect.area(length, width)}`);
-        console.log(`Perimeter of rectangle: ${rect.perimeter(length, width)}`);
-    }
+    rect(length, width, (err, rectangle) => {
+        if (err){
+            console.log('ERROR:', err.message);    
+        } else {
+            console.log(`Area of rectangle with dimensions ${length}, ${width} is: ${rectangle.area()}`);
+            console.log(`Perimeter of rectangle with dimensions ${length}, ${width} is: ${rectangle.perimeter()}`);
+        }
+    });
+    console.log('This statement is logged after the rect()');
 }
 
 solveRect(2, 4);
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,22 @@
-exports.perimeter = (x,y) => 2 * (x + y);
-exports.area = (x,y) => x * y;
+module.exports = (x, y, callback) => {
+    if ( x <= 0 || y <= 0){
+        callback(new Error(`Rectangle dimensions must be greated than zero. Received: ${x}, ${y}`));
+    } else {
+        setTimeout(() => 
+            callback(null, {
+                perimeter: () => 2 * (x + y),
+                area: () => x * y
+            }),
+            2000
+        );
+    }
+}
+
+//the callback param is a function that will be passed in when the function is called 
+//error callback pattern is an expected convention among node developers
+//en this error callback pattern we will check for the error condition- if the agruments we recieved through the function params are invalid in some way
+//first thing the callback returns is an error object- which is a global function that contains info about an error
+//can access x and y params in the inner function bc closures
+
+// perimeter = (x,y) => 2 * (x + y);
+// area = (x,y) => x * y;
